7eb1072d1125e49b2586e3ccb92a3e0f398cf0dd
Cherkashyna Eva <94197390+EvaCherkashyna@users.noreply.github.com>
2023-08-17 14:51:39 +0300

    finished the 'about clients' section

--- src/components/AboutClients/AboutClients.jsx
+++ src/components/AboutClients/AboutClients.jsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import styles from "./aboutClients.module.scss"
-
+import checkmark from "../../images/svg/checkmark.svg"
 const image = require("../../images/aboutClients.png")
 const AboutClients = () => {
   return (
-    <section>
-      <div>
-      </div>
-      <h3>We know learning inside and out </h3>
+    <section className={styles.section}>
       <img src={image} alt="peoples" />
-      <p>We pride ourselves on developing training strategies with the learners in mind, ensuring your time and resources are well utilized.</p>
+<div className={styles.content}>
+      <h4 className={styles.content_title}>Our clients love the work we do </h4>
+      <p className={styles.content_description}>
+        We pride ourselves on developing training strategies with the learners in mind, ensuring your time and resources are well utilized.</p>
       <ul>
         <li className={styles.list_item}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-            <path d="M6.25 13.75L13.75 21.25L26.25 8.75" stroke="#E7D08C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          <p className={styles.item_paragraph}>We start each project with a careful analysiys of your
-            learning needs</p>
+          <img src={checkmark} width="30px" />
+          <p className={styles.item_paragraph}>
+            We start each project with a careful analysiys of your
+            learning needs
+          </p>
         </li>
         <li className={styles.list_item}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-            <path d="M6.25 13.75L13.75 21.25L26.25 8.75" stroke="#E7D08C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          <p className={styles.item_paragraph}>We constantly look for ways to save your time and money while exceeding your expectations</p>
+          <img src={checkmark} width="30px" />
+          <p className={styles.item_paragraph}>
+            We constantly look for ways to save your time and money while exceeding your expectations
+          </p>
         </li>
         <li className={styles.list_item}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-            <path d="M6.25 13.75L13.75 21.25L26.25 8.75" stroke="#E7D08C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          <p className={styles.item_paragraph}>We don't stop working until you are satisfied with the products
-            we deliver</p>
+          <img src={checkmark} width="30px" />
+          <p className={styles.item_paragraph}>
+            We don't stop working until you are satisfied with the products
+            we deliver
+          </p>
         </li>
       </ul>
+</div>
 
     </section>
   )
